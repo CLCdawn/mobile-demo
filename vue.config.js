41dd8d6a78fc7d8d2074a411df7377f599db14bc
宵乐 <xiaole.ljw@alibaba-inc.com>
2020-05-10 22:32:24 +0800

    feat: 编译目录设置

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ const IS_PROD = ['production', 'prod'].includes(process.env.NODE_ENV)
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path')
 module.exports = {
-  publicPath: IS_PROD ? process.env.VUE_APP_PUBLIC_PATH : './', // 默认'/'，部署应用包时的基本 URL
+  outputDir: 'docs',
+  publicPath: './', // 默认'/'，部署应用包时的基本 URL
   // outputDir: process.env.outputDir || 'dist', // 'dist', 生产环境构建文件的目录
   // assetsDir: "", // 相对于outputDir的静态资源(js、css、img、fonts)目录
   lintOnSave: false,
